38c64572ab8914ddde58cb320848ec2f3c258f40
Lorenzo Carrera <loolirer@gmail.com>
2025-05-29 02:09:30 -0300

    power management only run once

--- Software/start.sh
+++ Software/start.sh
@@ -7,9 +7,11 @@ sudo pigpiod > /dev/null 2>&1 || echo "[ERROR] Pigpio Daemon Failed"
 # Activate python virtual environment
 source $HOME/.venv/bin/activate > /dev/null 2>&1 || echo "[ERROR] Venv Activation Failed"
 
-# Run power management script, it stays running in the background
-# even if the user logs out, avoiding hangups
-sudo python "$HOME/GoRASGo/Software/Python/gopigo3_power.py" > /dev/null 2>&1 || echo "[ERROR] GoPiGo3 Power Failed" &
+# Run power management script in background only once per boot
+if [ ! -f /tmp/gopigo3_power.lock ]; then
+    nohup sudo python "$HOME/GoRASGo/Software/Python/gopigo3_power.py" > /dev/null 2>&1 &
+    touch /tmp/gopigo3_power.lock
+fi
 
 # Change for development directory for convenience
 cd $HOME/GoRASGo/Software/Python/Tests/ > /dev/null 2>&1 || echo "[ERROR] Test Directory Not Found"
