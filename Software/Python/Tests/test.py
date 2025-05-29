9675e7a9cde4d783d95004539b0b898cbfd39e0f
Lorenzo Carrera <loolirer@gmail.com>
2025-05-29 01:58:05 -0300

    add example

---
+++ Software/Python/Tests/test.py
@@ -0,0 +1,12 @@
+import time
+from gopigo3 import GoPiGo3
+
+gpg = GoPiGo3()
+
+gpg.set_motor_power(gpg.MOTOR_LEFT, 100)
+gpg.set_motor_power(gpg.MOTOR_RIGHT, 100)
+
+time.sleep(2)
+
+gpg.set_motor_power(gpg.MOTOR_LEFT, 0)
+gpg.set_motor_power(gpg.MOTOR_RIGHT, 0)
