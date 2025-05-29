e0f1f80e59968b824413e1f8406da29faf3b12a6
Lorenzo Carrera <loolirer@gmail.com>
2025-05-29 00:07:53 -0300

    fix paths

--- Software/start.sh
+++ Software/start.sh
@@ -2,11 +2,11 @@
 sudo pigpiod
 
 # Activate python virtual environment
-source ./.venv/bin/activate
+source ~/.venv/bin/activate
 
 # Run power management script, it stays running in the background
 # even if the user logs out, avoiding hangups
-nohup python -m ./GoRASGo/Software/Python/gopigo3_power &
+nohup python -m ~/GoRASGo/Software/Python/gopigo3_power &
 
 # Change for development directory for convenience
-cd ./GoRASGo/Software/Python/Tests/
+cd ~/GoRASGo/Software/Python/Tests/
