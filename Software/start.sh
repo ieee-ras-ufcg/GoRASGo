c6e0484848028f249cadd45d835e7ce6493286f6
Lorenzo Carrera <loolirer@gmail.com>
2025-05-29 00:29:38 -0300

    add file extension in path

--- Software/start.sh
+++ Software/start.sh
@@ -6,7 +6,7 @@ source $HOME/.venv/bin/activate
 
 # Run power management script, it stays running in the background
 # even if the user logs out, avoiding hangups
-python $HOME/GoRASGo/Software/Python/gopigo3_power &
+python $HOME/GoRASGo/Software/Python/gopigo3_power.py &
 
 # Change for development directory for convenience
 cd $HOME/GoRASGo/Software/Python/Tests/
