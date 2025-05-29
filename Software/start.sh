861ed64e581e8190bee02abfb0f2435797ec9407
Lorenzo Carrera <loolirer@gmail.com>
2025-05-29 00:43:00 -0300

    refactor

--- Software/start.sh
+++ Software/start.sh
@@ -2,11 +2,11 @@
 sudo pigpiod
 
 # Activate python virtual environment
-source "$HOME/.venv/bin/activate"
+source $HOME/.venv/bin/activate
 
 # Run power management script, it stays running in the background
 # even if the user logs out, avoiding hangups
 sudo python $HOME/GoRASGo/Software/Python/gopigo3_power.py &
 
 # Change for development directory for convenience
-cd "$HOME/GoRASGo/Software/Python/Tests/"
+cd $HOME/GoRASGo/Software/Python/Tests/
