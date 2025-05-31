6031760ec38b9df9781ea0f441f00e3757792416
Lorenzo Carrera <loolirer@gmail.com>
2025-05-31 14:37:23 -0300

    refactor

--- Software/Python/Tests/test.py
+++ Software/Python/Tests/test.py
@@ -3,10 +3,10 @@ from gopigo3 import GoPiGo3
 
 gpg = GoPiGo3()
 
-gpg.set_motor_power(gpg.MOTOR_LEFT, 100)
-gpg.set_motor_power(gpg.MOTOR_RIGHT, 100)
+gpg.set_motor_power(gpg.MOTOR_LEFT, 127)
+gpg.set_motor_power(gpg.MOTOR_RIGHT, 127)
 
-time.sleep(2)
+time.sleep(1)
 
 gpg.set_motor_power(gpg.MOTOR_LEFT, 0)
 gpg.set_motor_power(gpg.MOTOR_RIGHT, 0)
