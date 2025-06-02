f1e2ff6a5cbecb49fd0ac6b63913dd5d140ddcd9
Lorenzo Carrera <loolirer@gmail.com>
2025-06-02 19:00:32 -0300

    add open loop control script

---
+++ Software/Python/Tests/open_loop_control.py
@@ -0,0 +1,32 @@
+import cv2
+import socket
+import numpy as np
+from gopigo3 import GoPiGo3
+
+gpg = GoPiGo3()
+
+try:
+    print("[INFO] Starting UDP client...")
+    gpg_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    gpg_socket.bind(("0.0.0.0", 25565))
+
+except:
+    print("[ERROR] Could not start UDP client")
+    exit()
+
+print("[INFO] UDP client set")
+print("[INFO] Receiving messages...")
+
+while cv2.waitKey(1) == -1:
+    # Receive UPD message
+    data, address = gpg_socket.recvfrom(1024) 
+
+    # Parse wheel velocities
+    phi_dot_L, phi_dot_R = map(int, map(float, data.decode().split(" ")))
+
+    # Limit velocity values
+    phi_dot_L, phi_dot_R = np.clip(-1000, 1000, [phi_dot_L, phi_dot_R])
+
+    # Set velocities to motors
+    gpg.set_motor_dps(gpg.MOTOR_LEFT, phi_dot_L)
+    gpg.set_motor_dps(gpg.MOTOR_RIGHT, phi_dot_R)
