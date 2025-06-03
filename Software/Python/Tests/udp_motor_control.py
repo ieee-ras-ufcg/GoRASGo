ac4e9529d53560400d578550d977dc92eb0b67f5
Lorenzo Carrera <loolirer@gmail.com>
2025-06-03 16:40:55 -0300

    tuning PID

--- Software/Python/Tests/udp_motor_control.py
+++ Software/Python/Tests/udp_motor_control.py
@@ -104,8 +104,8 @@ try:
         speed_R = PID_R.get_output(ref_speed_R - mea_speed_R)
 
         # Set velocities to motors
-        gpg.set_motor_dps(gpg.MOTOR_LEFT, ref_speed_L)
-        gpg.set_motor_dps(gpg.MOTOR_RIGHT, ref_speed_R)
+        gpg.set_motor_dps(gpg.MOTOR_LEFT, speed_L)
+        gpg.set_motor_dps(gpg.MOTOR_RIGHT, speed_R)
 
         # Send data to simulation
         gpg_socket.sendto(
