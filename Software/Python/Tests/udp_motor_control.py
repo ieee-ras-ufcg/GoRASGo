fa4a5d686f9e58c53ae5d902896cf4c31a36bd51
Lorenzo Carrera <loolirer@gmail.com>
2025-06-02 22:53:31 -0300

    refactor

--- Software/Python/Tests/udp_motor_control.py
+++ Software/Python/Tests/udp_motor_control.py
@@ -48,7 +48,7 @@ try:
         gpg.set_motor_dps(gpg.MOTOR_RIGHT, phi_dot_R)
 
         print(
-            f"L: ({encoder_left:+04.0f}) | R: ({encoder_right:+04.0f} | t: ({(finish - start):+.2e})"
+            f"L: ({last_encoder_left:+04.0f}) | R: ({encoder_left:+04.0f}) | t: ({(finish - start):+.2e})"
         )
 
         # Update variables
