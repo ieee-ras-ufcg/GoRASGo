32b594adf062887716a690376e7269f55487e8d4
Lorenzo Carrera <loolirer@gmail.com>
2025-06-02 23:03:40 -0300

    Squashed commit of the following:
    commit 5c298cc542a2e2fd598f7c8285e893189ca9cdd4
    Author: Lorenzo Carrera <loolirer@gmail.com>
    Date:   Mon Jun 2 23:01:38 2025 -0300
    
        refactor
    
    commit fa4a5d686f9e58c53ae5d902896cf4c31a36bd51
    Author: Lorenzo Carrera <loolirer@gmail.com>
    Date:   Mon Jun 2 22:53:31 2025 -0300
    
        refactor
    
    commit 9c67eb005a04275c8a09f4f59055e2fd4f3babdb
    Author: Lorenzo Carrera <loolirer@gmail.com>
    Date:   Mon Jun 2 22:52:17 2025 -0300
    
        refactor
    
    commit 7dab2097d09ddd69d91308f7b1722067370729bf
    Author: Lorenzo Carrera <loolirer@gmail.com>
    Date:   Mon Jun 2 22:49:22 2025 -0300
    
        refactor
    
    commit b65bd87a37e65c1f61905887778067671875e49d
    Author: Lorenzo Carrera <loolirer@gmail.com>
    Date:   Mon Jun 2 22:43:20 2025 -0300
    
        refactor
    
    commit 0db4eaf61cdeefd4601bde08b1ade8c3703ed4e8
    Author: Lorenzo Carrera <loolirer@gmail.com>
    Date:   Mon Jun 2 22:40:33 2025 -0300
    
        refactor
    
    commit f4ec3043358f927dc52218d128281870e468e89a
    Author: Lorenzo Carrera <loolirer@gmail.com>
    Date:   Mon Jun 2 22:38:36 2025 -0300
    
        refactor
    
    commit 5c59c7cf71387bf1ce5121cf5a0928e6f9ddaabc
    Author: Lorenzo Carrera <loolirer@gmail.com>
    Date:   Mon Jun 2 22:33:58 2025 -0300
    
        refactor
    
    commit 3c14a7aaf8b4aad3b2cbe23f4af1a0811b1c526f
    Author: Lorenzo Carrera <loolirer@gmail.com>
    Date:   Mon Jun 2 22:31:36 2025 -0300
    
        refactor
    
    commit 05c143f0d44fe7c00042c6e9238018a8c076d7c3
    Author: Lorenzo Carrera <loolirer@gmail.com>
    Date:   Mon Jun 2 22:30:34 2025 -0300
    
        refactor
    
    commit 11a3209dec164cef4d04d2e80ca8c0068c9b018e
    Author: Lorenzo Carrera <loolirer@gmail.com>
    Date:   Mon Jun 2 22:28:35 2025 -0300
    
        refactor
    
    commit 54dc621a660f7683bcf920900d2528263c219e26
    Author: Lorenzo Carrera <loolirer@gmail.com>
    Date:   Mon Jun 2 22:25:59 2025 -0300
    
        refactor
    
    commit 7b4bf21e2e2cd046f8a6f9d8d29d427dc3089bf8
    Author: Lorenzo Carrera <loolirer@gmail.com>
    Date:   Mon Jun 2 22:19:54 2025 -0300
    
        refactor
    
    commit 7e506251acd2ad9d050003fd3e6ab5e5aec3adfc
    Author: Lorenzo Carrera <loolirer@gmail.com>
    Date:   Mon Jun 2 22:11:15 2025 -0300
    
        refactor
    
    commit 0b6f8439c492b312e34396d68d1cff34389e7860
    Author: Lorenzo Carrera <loolirer@gmail.com>
    Date:   Mon Jun 2 22:10:14 2025 -0300
    
        refactor
    
    commit 765e7f938ffdd6901021ad720e2f8e2e78f4b517
    Author: Lorenzo Carrera <loolirer@gmail.com>
    Date:   Mon Jun 2 21:50:54 2025 -0300
    
        refactor
    
    commit 882faecf2d86b3b34fc44c46ef733097b0bdb17f
    Author: Lorenzo Carrera <loolirer@gmail.com>
    Date:   Mon Jun 2 21:47:09 2025 -0300
    
        model refactor
    
    commit a1a08c3938a278f25204ef5b5f20174f55af9ac2
    Author: Lorenzo Carrera <loolirer@gmail.com>
    Date:   Mon Jun 2 19:16:17 2025 -0300
    
        refactor
    
    commit 569ec9246bd7f2c804e02d0afc17d70ff7daa18f
    Author: Lorenzo Carrera <loolirer@gmail.com>
    Date:   Mon Jun 2 19:14:22 2025 -0300
    
        refactor

---
+++ Software/Python/Tests/udp_motor_control.py
@@ -0,0 +1,41 @@
+import socket
+import numpy as np
+from gopigo3 import GoPiGo3
+
+gpg = GoPiGo3()
+gpg.reset_all()
+
+try:
+    print("[INFO] Starting UDP client...")
+    gpg_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    gpg_socket.bind(("0.0.0.0", 25565))
+
+except:
+    print("[ERROR] Could not start UDP client")
+    exit()
+
+print("[INFO] UDP client set")
+
+try:
+    print("[INFO] Running... Press Ctrl+C to stop")
+
+    while True:
+        # Receive UPD message
+        data, address = gpg_socket.recvfrom(1024)
+
+        # Parse wheel velocities
+        phi_dot_L, phi_dot_R = map(int, map(float, data.decode().split(" ")))
+
+        # Limit velocity values
+        phi_dot_L, phi_dot_R = np.clip(-1000, 1000, [phi_dot_L, phi_dot_R])
+
+        # Set velocities to motors
+        gpg.set_motor_dps(gpg.MOTOR_LEFT, phi_dot_L)
+        gpg.set_motor_dps(gpg.MOTOR_RIGHT, phi_dot_R)
+
+
+except KeyboardInterrupt:
+    print("\n[INFO] Execution stopped externally")
+    print("[INFO] Motors shutdown")
+    gpg.set_motor_dps(gpg.MOTOR_LEFT, 0)
+    gpg.set_motor_dps(gpg.MOTOR_RIGHT, 0)
