8eef9cbe351eb90fa759d91d81db3f0d2b68bb34
Lorenzo Carrera <loolirer@gmail.com>
2025-06-03 16:51:21 -0300

    add lowpass

--- Software/Python/Tests/udp_motor_control.py
+++ Software/Python/Tests/udp_motor_control.py
@@ -4,7 +4,40 @@ import numpy as np
 from gopigo3 import GoPiGo3
 
 
-# PID Controller
+class FirstOrderLowPass:
+    def __init__(
+        self,
+        fc,  # Cutoff frequency, in Hz
+        fs,  # Sampling frequency, in Hz
+    ):
+        self.Ts = 1.0 / fs  # Sampling time
+        self.tau = 1.0 / (2 * np.pi * fc)  # Filter time constant
+        self.alpha = self.Ts / (self.tau + self.Ts)
+        self.y_prev = 0.0
+
+    def filter(self, x):
+        y = self.alpha * x + (1 - self.alpha) * self.y_prev
+        self.y_prev = y
+
+        return y
+
+
+class LowPassFilter:
+    def __init__(
+        self,
+        order,  # Order of the filter
+        fc,  # Cutoff frequency, in Hz
+        fs,  # Sampling frequency, in Hz
+    ):
+        self.stages = [FirstOrderLowPass(fc, fs) for _ in range(order)]
+
+    def filter(self, x):
+        for stage in self.stages:
+            x = stage.filter(x)
+
+        return x
+
+
 class PID:
     def __init__(
         self,
@@ -56,8 +89,10 @@ gpg = GoPiGo3()
 gpg.reset_all()
 
 PID_L = PID(K_p=2.0, K_i=2.0, K_d=0.0)
+LPF_L = LowPassFilter(5, 30, 1/PID_L.dt)
 
 PID_R = PID(K_p=2.0, K_i=2.0, K_d=0.0)
+LPF_R = LowPassFilter(5, 30, 1/PID_R.dt)
 
 try:
     print("[INFO] Starting UDP client...")
@@ -96,8 +131,9 @@ try:
         # Estimate actual speed
         encoder_L = gpg.get_motor_encoder(gpg.MOTOR_LEFT)
         encoder_R = gpg.get_motor_encoder(gpg.MOTOR_RIGHT)
-        mea_speed_L = (encoder_L - last_encoder_left) / (finish - start)
-        mea_speed_R = (encoder_R - last_encoder_right) / (finish - start)
+
+        mea_speed_L = LPF_L.filter((encoder_L - last_encoder_left) / (finish - start))
+        mea_speed_R = LPF_R.filter((encoder_R - last_encoder_right) / (finish - start))
 
         # Compute control action
         speed_L = PID_L.get_output(ref_speed_L - mea_speed_L)
