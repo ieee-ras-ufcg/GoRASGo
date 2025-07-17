4b4b120c0f35e2c8523a32336ca650c37311e900
Lorenzo Carrera <loolirer@gmail.com>
2025-07-16 21:55:10 -0300

    automatically get local machine ip

--- Software/Python/Tests/teleop_open_loop.py
+++ Software/Python/Tests/teleop_open_loop.py
@@ -11,7 +11,7 @@ try:
     gpg_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     gpg_socket.bind(("0.0.0.0", 25565))
 
-    sim_address = ("192.168.0.101", 25565)
+    sim_address = (socket.gethostbyname(socket.gethostname()), 25565)
 
 except Exception as e:
     print("[ERROR] Could not start UDP client")
